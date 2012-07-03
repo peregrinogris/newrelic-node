10e6826eebb0fada3a1b0e0c1cdfaf418fbc05c2
Matias Rege <matias.rege@mercadolibre.com>
2012-07-03 17:40:33 -0300

    paarametro puerto y buffer por parametro a java

--- newrelic/src/Monitor.java
+++ newrelic/src/Monitor.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
+import java.util.Arrays;
 
 import org.apache.mina.common.ByteBuffer;
 import org.apache.mina.common.IoAcceptor;
@@ -13,28 +14,26 @@ import org.apache.mina.transport.socket.nio.SocketAcceptorConfig;
 /**
  * Created by: Matias Rege
  */
-public class Monitor implements Runnable {
-	private static final int PORT = 9999;
-	
-	private Monitor() throws IOException {
+public class Monitor  {
+		
+	private Monitor(int port, int buffer) throws IOException {
 		ByteBuffer.setUseDirectBuffers(false);
 		ByteBuffer.setAllocator(new SimpleByteBufferAllocator());
 
 		IoAcceptor acceptor = new SocketAcceptor();
 
 		SocketAcceptorConfig cfg = new SocketAcceptorConfig();
-		cfg.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
+		TextLineCodecFactory factory = new TextLineCodecFactory(Charset.forName("UTF-8"));
+		factory.setDecoderMaxLineLength(buffer);
+		cfg.getFilterChain().addLast("codec", new ProtocolCodecFilter(factory));
 
-		acceptor.bind(new InetSocketAddress(PORT), new MonitorHandler(), cfg);
-		System.out.println("Server started.");
+		acceptor.bind(new InetSocketAddress(port), new MonitorHandler(), cfg);
 	}
 
 	public static void main(String[] args) throws IOException {
-		new Monitor().run();
-	}
-
-	public void run() {
-
+		System.out.println("Iniciando monitor con parametros: "+Arrays.toString(args));
+		new Monitor(Integer.valueOf(args[0]), Integer.valueOf(args[1]));
+		System.out.println("Server started.");
 	}
 }
 
